42ddcf67e33228f69330ebaeeabd29eba3d6b444
mohamedKamal <https://github.com/Mohamed-kamal1>
2022-12-25 07:36:04 +0200

    add the project

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -22,4 +22,43 @@ export class AppComponent {
 
     }
   }
+  click(click: string) {
+
+    document.getElementById("inbox")!.style.zIndex="-1";
+    document.getElementById("starred")!.style.zIndex="-1";
+    document.getElementById("snoozed")!.style.zIndex = "-1";
+    document.getElementById("sent")!.style.zIndex = "-1";
+    document.getElementById("draft")!.style.zIndex = "-1";
+    document.getElementById("trash")!.style.zIndex = "-1";
+    document.getElementById("person")!.style.zIndex = "-1";
+    document.getElementById("lable")!.style.zIndex = "-1";
+    document.getElementById("compose")!.style.zIndex = "-1";
+    if (click == "inbox") {
+      document.getElementById("inbox")!.style.zIndex = "1";
+    }
+    else if (click == "starred") {
+      document.getElementById("starred")!.style.zIndex = "1";
+    }
+    else if (click == "snoozed") {
+      document.getElementById("snoozed")!.style.zIndex = "1";
+    }
+    else if (click == "sent") {
+      document.getElementById("sent")!.style.zIndex = "1";
+    }
+    else if (click == "draft") {
+      document.getElementById("draft")!.style.zIndex = "1";
+    }
+    else if (click == "trash") {
+      document.getElementById("trash")!.style.zIndex = "1";
+    }
+    else if (click == "person") {
+      document.getElementById("person")!.style.zIndex = "1";
+    }
+    else if (click == "lable") {
+      document.getElementById("lable")!.style.zIndex = "1";
+    }
+    else if (click == "compose") {
+      document.getElementById("compose")!.style.zIndex = "1";
+    }
+  }
 }
