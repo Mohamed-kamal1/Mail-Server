26f382d72f38aac22283729d428afc2af11ad7ac
mohamedKamal <https://github.com/Mohamed-kamal1>
2022-12-25 06:16:26 +0200

    add the project

---
+++ Backend/E-mail/src/main/java/com/example/Email/EMailApplication.java
@@ -0,0 +1,14 @@
+package com.example.Email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMailApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EMailApplication.class, args);
+	}
+
+}
---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css','./bootstrap.css']
+})
+export class AppComponent {
+  title = 'Mail-Server';
+
+  select = false;
+
+
+
+  select_menu() {
+    this.select = !this.select;
+    if (this.select) {
+      document.getElementById("select")!.style.display = "block";
+    }
+    else {
+      document.getElementById("select")!.style.display = "none";
+
+    }
+  }
+}
