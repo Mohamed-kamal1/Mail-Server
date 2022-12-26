90058e1b1a604ea17ba2e6e92756b06a84d6fc6f
mohamedKamal <https://github.com/Mohamed-kamal1>
2022-12-26 19:25:17 +0200

    update

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,62 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css','./bootstrap.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Mail-Server';
-
-  select = false;
-
-
-
-  select_menu() {
-    this.select = !this.select;
-    if (this.select) {
-      document.getElementById("select")!.style.display = "block";
-    }
-    else {
-      document.getElementById("select")!.style.display = "none";
-
-    }
-  }
-  click(click: string) {
-
-    document.getElementById("inbox")!.style.zIndex="-1";
-    document.getElementById("starred")!.style.zIndex="-1";
-    document.getElementById("snoozed")!.style.zIndex = "-1";
-    document.getElementById("sent")!.style.zIndex = "-1";
-    document.getElementById("draft")!.style.zIndex = "-1";
-    document.getElementById("trash")!.style.zIndex = "-1";
-    document.getElementById("person")!.style.zIndex = "-1";
-    document.getElementById("lable")!.style.zIndex = "-1";
-    document.getElementById("compose")!.style.zIndex = "-1";
-    if (click == "inbox") {
-      document.getElementById("inbox")!.style.zIndex = "1";
-    }
-    else if (click == "starred") {
-      document.getElementById("starred")!.style.zIndex = "1";
-    }
-    else if (click == "snoozed") {
-      document.getElementById("snoozed")!.style.zIndex = "1";
-    }
-    else if (click == "sent") {
-      document.getElementById("sent")!.style.zIndex = "1";
-    }
-    else if (click == "draft") {
-      document.getElementById("draft")!.style.zIndex = "1";
-    }
-    else if (click == "trash") {
-      document.getElementById("trash")!.style.zIndex = "1";
-    }
-    else if (click == "person") {
-      document.getElementById("person")!.style.zIndex = "1";
-    }
-    else if (click == "lable") {
-      document.getElementById("lable")!.style.zIndex = "1";
-    }
-    else if (click == "compose") {
-      document.getElementById("compose")!.style.zIndex = "1";
-    }
-  }
 }
---
+++ Frontend/src/app/home/home.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  title = 'Mail-Server';
+
+  select = false;
+
+
+
+  select_menu() {
+    this.select = !this.select;
+    if (this.select) {
+      document.getElementById("select")!.style.display = "block";
+    }
+    else {
+      document.getElementById("select")!.style.display = "none";
+
+    }
+  }
+  click(click: string) {
+
+    document.getElementById("inbox")!.style.zIndex = "-1";
+    document.getElementById("starred")!.style.zIndex = "-1";
+    document.getElementById("snoozed")!.style.zIndex = "-1";
+    document.getElementById("sent")!.style.zIndex = "-1";
+    document.getElementById("draft")!.style.zIndex = "-1";
+    document.getElementById("trash")!.style.zIndex = "-1";
+    document.getElementById("person")!.style.zIndex = "-1";
+    document.getElementById("lable")!.style.zIndex = "-1";
+    document.getElementById("compose")!.style.zIndex = "-1";
+    if (click == "inbox") {
+      document.getElementById("inbox")!.style.zIndex = "1";
+    }
+    else if (click == "starred") {
+      document.getElementById("starred")!.style.zIndex = "1";
+    }
+    else if (click == "snoozed") {
+      document.getElementById("snoozed")!.style.zIndex = "1";
+    }
+    else if (click == "sent") {
+      document.getElementById("sent")!.style.zIndex = "1";
+    }
+    else if (click == "draft") {
+      document.getElementById("draft")!.style.zIndex = "1";
+    }
+    else if (click == "trash") {
+      document.getElementById("trash")!.style.zIndex = "1";
+    }
+    else if (click == "person") {
+      document.getElementById("person")!.style.zIndex = "1";
+    }
+    else if (click == "lable") {
+      document.getElementById("lable")!.style.zIndex = "1";
+    }
+    else if (click == "compose") {
+      document.getElementById("compose")!.style.zIndex = "1";
+    }
+  }
+
+}
